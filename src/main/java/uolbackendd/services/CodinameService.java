d8d1e8219b76952441d2ee1b7d1a4d4703811022
Yuri Neves <yurineves1934@gmail.com>
2023-10-24 00:31:44 -0300

    feat: create cacheable data

--- src/main/java/uolbackendd/services/CodinameService.java
+++ src/main/java/uolbackendd/services/CodinameService.java
@@ -7,6 +7,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -19,6 +20,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 
 import jakarta.annotation.PostConstruct;
+
 import lombok.Getter;
 
 @Getter
@@ -31,45 +33,53 @@ public class CodinameService {
   @Autowired
   private Environment env;
 
-  private List<String> avangersCodinameList = new ArrayList<>();
-  private List<String> justiceLeagueList = new ArrayList<>();
+  private List<String> avangersCodinameList;
+  private List<String> justiceLeagueList;
 
   ObjectMapper objectMapper = new ObjectMapper();
 
   @PostConstruct
-  public void loadJsonData() {
+  @Cacheable("codenames")
+  public void loadAllData() {
     try {
-      String codenameResponse = restTemplate.getForObject(env.getProperty("avangers"), String.class);
-      JsonNode jsonNode = objectMapper.readTree(codenameResponse);
-
-      ArrayNode avengers = (ArrayNode) jsonNode.get("vingadores");
-
-      for (JsonNode item : avengers) {
-        this.avangersCodinameList.add(item.get("codinome").asText());
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
-
-  @PostConstruct
-  public void loadXmlData() {
-    try {
-      DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-      DocumentBuilder builder = factory.newDocumentBuilder();
-      Document document = builder.parse(env.getProperty("justice.league"));
-
-      NodeList codenameList = document.getElementsByTagName("codinome");
-
-      for (int item = 0; item < codenameList.getLength(); item++) {
-        Element codinameElement = (Element) codenameList.item(item);
-        String codename = codinameElement.getTextContent();
-        this.justiceLeagueList.add(codename);
-      }
-
+      this.avangersCodinameList = loadJsonData();
+      this.justiceLeagueList = loadXmlData();
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
+  }
+
+  private List<String> loadJsonData() throws Exception {
+    String codenameResponse = restTemplate.getForObject(env.getProperty("avangers"), String.class);
+    JsonNode jsonNode = objectMapper.readTree(codenameResponse);
+
+    ArrayNode avengers = (ArrayNode) jsonNode.get("vingadores");
+
+    List<String> codenames = new ArrayList<>();
+
+    for (JsonNode item : avengers) {
+      codenames.add(item.get("codinome").asText());
+    }
+
+    return codenames;
+  }
+
+  private List<String> loadXmlData() throws Exception {
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    DocumentBuilder builder = factory.newDocumentBuilder();
+    Document document = builder.parse(env.getProperty("justice.league"));
+
+    NodeList codenameList = document.getElementsByTagName("codinome");
+
+    List<String> codenames = new ArrayList<>();
+
+    for (int item = 0; item < codenameList.getLength(); item++) {
+      Element codinameElement = (Element) codenameList.item(item);
+      String codename = codinameElement.getTextContent();
+      codenames.add(codename);
+    }
+
+    return codenames;
   }
 
 }
